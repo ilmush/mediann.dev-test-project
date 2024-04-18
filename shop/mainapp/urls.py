619d7fb982976350b4c4c430503bbe1e87cd6812
ilmush <zainukovail@yandex.ru>
2024-04-18 21:47:44 +0300

    authorization using JWT

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,7 +24,12 @@ from rest_framework.routers import SimpleRouter
 from .yasg import urlpatterns as doc_urls
 from shop.views import *
 
-# from ..shop.views import ProductViewSet
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = SimpleRouter()
 
@@ -36,12 +41,15 @@ urlpatterns = [
     path('cart/', CartView.as_view()),
     path('add-to-cart/<str:slug>/', AddToCartView.as_view()),
     path('create-order/', OrderApiView.as_view()),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
 
 urlpatterns += router.urls
 urlpatterns += doc_urls
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
